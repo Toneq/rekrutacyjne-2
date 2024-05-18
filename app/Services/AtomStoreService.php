fb891f6ec3cd27735f27e5c11ea29262c20118d6
Tobiasz Tonn <toneq@tlen.pl>
2024-05-18 18:22:10 +0200

    dodanie kodu dla AtomStoreService - zapis produktów

--- app/Services/AtomStoreService.php
+++ app/Services/AtomStoreService.php
@@ -22,4 +22,45 @@ class AtomStoreService
         $this->client = new \SoapClient($this->wsdl);
         $this->authenticate = ['login' => $this->username, 'password' => $this->password];
     }
+
+    public function setProduct($payload)
+    {
+        $attributes = '';
+        foreach ($payload['attributes'] as $key => $value) {
+            $attributes .= '
+                <attribute>
+                    <code>
+                        ' . $key . '
+                    </code>
+                    <name>
+                        <![CDATA[ ' . $key . ' ]]>
+                    </name>
+                    <code_value>
+                        ' . $key . '
+                    </code_value>
+                    <value>
+                        <![CDATA[ ' . $value . ' ]]>
+                    </value>
+                </attribute>
+            ';
+        }
+
+        $xml = '
+        <products>
+            <product>
+                <code>' . $payload["code"] . '</code>
+                <update>1</update>
+                <product_name>
+                    <![CDATA[ ' . $payload["name"] . ' ]]>
+                </product_name>
+                <price_brutto>' . $payload["price"] . '</price_brutto>
+                <vat_rate>' . $payload["vat_rate"] . '</vat_rate>
+                <quantity>' . $payload["stock"] . '</quantity>
+                <attributes>' . $attributes . '</attributes>
+            </product>
+        </products>
+        ';
+        $message = ['xml' => $xml];
+        $response = $this->client->SetProducts($this->authenticate, $message);
+    }
 }
